649f97dd33db17da3de8d644750ac73e09bbe9bd
CodeWizardDiSar <saridakisbitos@gmail.com>
2019-12-06 16:12:21 +0200

    m

--- 1_exercises/1_source/1_generalStuff/0_general.hs
+++ 1_exercises/1_source/1_generalStuff/0_general.hs
@@ -10,25 +10,27 @@ import Types
 --Capital letters in comments explain names
 
 --paths
-vek=(dkd++"/ver")   ::FPa--VErsion Keeper
-dkd="../0_data"     ::FPa--Data Keeper Dir
-dkp=(dkd++"/data")  ::FPa--Data Keeper file Prefix
-tvk=(dkd++"/verNew")::FPa--Temp Version Keeper
+vek=(dkd++"/ver")   ::FPA--VErsion Keeper
+dkd="../0_data"     ::FPA--Data Keeper Dir
+dkp=(dkd++"/data")  ::FPA--Data Keeper file Prefix
+tvk=(dkd++"/verTmp")::FPA--Temporary Version Keeper
 
 --renaming
-rdf=readFile     ::FPa->IOS     --ReaD File
-cns=readFile     ::FPa->IOS     --CoNtentS
-wrf=writeFile    ::FPa->STR->IOU--WRite File
+rdf=readFile     ::FPA->IOS     --ReaD File
+cns=readFile     ::FPA->IOS     --CoNtentS
+wrf=writeFile    ::FPA->STR->IOU--WRite File
 pst=putStrLn     ::STR->IOU     --Print STring
-wim=(return)     ::Mon m=>a->m a--Wrap In Monad
-exi=doesFileExist::FPa->IOB     --file EXIsts?
+wim=(return)     ::MON m=>a->m a--Wrap In Monad
+exi=doesFileExist::FPA->IOB     --file EXIsts?
 
 --had to be done
-nli=pst ""                     ::IOU     --New LIne
+nli=pst ""                     ::IOU               --New LIne
 nls=(\case 0->wim ()
-           i->nli>>((i-1)&nls))::Int->IOU--New LineS
-wnl=(\p->nli>>p>>nli)          ::IOU->IOU--Wrap In Newlines
-aos=(read>>>(+1)>>>show)       ::STR->STR--Add One to String
+           i->nli>>((i-1)&nls))::Int->IOU          --New LineS
+wnl=(\p->nli>>p>>nli)          ::IOU->IOU          --Wrap In Newlines
+aos=(read>>>(+1)>>>show)       ::STR->STR          --Add One to String
+pss=mapM_ pst                  ::Sts->IOU          --Print StringS
+mco=(\f->map f>>>concat)       ::(a->[b])->[a]->[b]--Map and Concat
 
 --if there is a version keeper
 --read the version from it 
@@ -36,7 +38,7 @@ aos=(read>>>(+1)>>>show)       ::STR->STR--Add One to String
 ver=(vke>>=bca rvk ww0)::IOS         --VERsion
 vke=vek&exi            ::IOB         --Version Keeper Exists?
 bca=(\a b->
-  \case True ->a;_->b) ::a->a->Boo->a--Bool CAse (if then else)
+  \case True ->a;_->b) ::a->a->BOO->a--Bool CAse (if then else)
 rvk=rdf vek            ::IOS         --Read Version Keeper
 ww0=wwm "0"            ::IOS         --Write to vk and Wrap in monad "0"
 wwm=(\s->wvk s>>wim s) ::STR->IOS    --Write to vk and Wrap in Monad
@@ -45,8 +47,8 @@ wvk=wrf vek            ::STR->IOU    --Write to Version Keeper
 --get version
 --add one to it
 --write new version to version keeper
-upv=a1V>>rev             ::IOU--UPdate Version
-a1V=(ver>>=awt)          ::IOU--Add 1 to Version
+upv=a1v>>rev             ::IOU--UPdate Version
+a1v=(ver>>=awt)          ::IOU--Add 1 to Version
 awt=(aos>>>wtv)          ::STR->IOU--Add 1 and Write to Temp vk
 wtv=wrf tvk              ::STR->IOU--Write to Temp Version keeper
 rev=renameFile tvk vek   ::IOU--REname temp Vk to vk
@@ -56,6 +58,3 @@ apw=((dkp++)>>>wim)      ::STR->IOS--Append to Prefix and Wrap in monad
 
 nvf=(ver>>=aaw)          ::IOF     --Next Version File path
 aaw=(aos>>>apw)          ::STR->IOS--Add 1 Append to prefix and Wrap
-
-pss=mapM_ pst            ::Sts->IOU--Print StringS
-mco=(\f->map f>>>concat) ::(a->[b])->[a]->[b]--Map and Concat
--- 1_exercises/1_source/1_generalStuff/1_types.hs
+++ 1_exercises/1_source/1_generalStuff/1_types.hs
@@ -9,28 +9,28 @@ class FromFileLine   a where fromFLine ::Lin->a
 class ToFileStr      a where toFStr    ::a  ->STR
 class ToStrsForDecor a where toStrsD   ::a  ->Sts
 
-type STR=String    --String 
-type FPa=FilePath  --File Path
-type Boo=Bool      --Boolean
-type Mon=Monad     --Monad
-type IOU=IO ()     --IO unit
-type IOS=IO STR    --IO String 
-type IOB=IO Boo    --IO Boolean 
-type IOF=IO FPa    --IO File path
-type Day=Int       -- 
-type Mnt=Int       -- 
-type Siz=Int       -- 
-type SNa=STR       -- 
-type ENa=STR       -- 
-type Lin=STR       -- 
-type Dcr=Sts       -- 
-type MEN=Maybe ENa -- 
-type DEs=[DEx]     -- 
-type TEs=[TEx]     -- 
-type Sts=[STR]     -- 
-type Sbs=[Sub]     -- 
-type Lns=[Lin]     -- 
-type Dat=(Day,Mnt) -- 
+type STR=String   --STRing 
+type FPA=FilePath --File PAth
+type BOO=Bool     --BOOlean
+type MON=Monad    --MONad
+type IOU=IO ()    --IO Unit
+type IOS=IO STR   --IO String 
+type IOB=IO BOO   --IO Boolean 
+type IOF=IO FPA   --IO File path
+type DAY=Int      --DAY
+type MNT=Int      --MoNTh
+type SIZ=Int      --SIZe
+type SNa=STR      --Subject Name
+type ENa=STR      -- 
+type Lin=STR      -- 
+type Dcr=Sts      -- 
+type MEN=Maybe ENa-- 
+type DEs=[DEx]    -- 
+type TEs=[TEx]    -- 
+type Sts=[STR]    -- 
+type Sbs=[Sub]    -- 
+type Lns=[Lin]    -- 
+type Dat=(DAY,MNT)-- 
 
 data DEx=DE MEN Int
 data TEx=TE MEN Int Dat
