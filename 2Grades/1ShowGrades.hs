0f8637001d64f0eb4b77eeb29b8f8124933fde9b
gnostis <saridakisbitos@gmail.com>
2020-10-03 23:51:58 +0300

    I am an automated comment

--- 2Grades/1ShowGrades.hs
+++ 2Grades/1ShowGrades.hs
@@ -5,12 +5,14 @@ from x = x
 element n from x = from x!!(n-1)
 gradesCsv = "/home/gnostis/Dropbox/2_Hobbies/1_Prog/1_Projects/1_UniRelated/2_Grades/grades.csv"
 
-kormos = filter (\x -> element 1 from x == "k")
-roes   = filter (\x -> element 1 from x == "r") 
-passed = filter (\x -> element 4 from x /= "0")
+kormos     = filter (\x -> element 1 from x == "k")
+roes       = filter (\x -> element 1 from x == "r") 
+elefthera  = filter (\x -> element 1 from x == "e") 
+passed     = filter (\x -> element 4 from x /= "0")
 
-passedKormos = passed . kormos
-passedRoes   = passed . roes
+passedKormos    = passed . kormos
+passedRoes      = passed . roes
+passedElefthera = passed . elefthera
 
 semester n = filter (\x -> element 2 from x == show n) 
 roh      r = filter (\x -> element 2 from x == r     )
@@ -26,25 +28,34 @@ titledSemester n = (concat ["\n",show n,"ο εξάμηνο:\n\n"] ++) . nicerSem
 nicerRoh    = toNicer . roh
 titledRoh r = (concat ["\nΡοή ",r,":\n\n"] ++) . nicerRoh r 
 
+titledElefthera = (concat ["\nΕλεύθερα:\n\n"] ++) . toNicer elefthera
+
 subjectsOf = map (splitOn ",") . lines
 nicerKormos csv = concat $ map (\n -> titledSemester n (subjectsOf csv)) [1,2,3,4,5]
-nicerRoes csv = concat $ map (\s -> titledRoh s (subjectsOf csv)) ["Υ" ,"Λ","Μ"]
+nicerRoes csv = concat $ map (\s -> titledRoh s (subjectsOf csv)) ["Υ","Λ","Μ","Φ"]
+nicerElefthera csv = titledElefthera (subjectsOf csv)
 
-moreInfo csv = 
-  "\nΓενικές Πληροφορίες:\n\n" ++
-  "\tΠερασμένα Κορμού: "  ++ (show $ l_p_k) ++
-  "\n\tΑπομένουν από Κορμό: "++ (show $ 35 - l_p_k) ++
-  "\n\tΠερασμένα Ροών: " ++ (show $ l_p_r) ++
-  "\n\tΑπομένουν από Ροές: " ++ (show $ 25 - l_p_r) ++
-  (concat (map grade [5,6,7,8,9,10])) ++
-  "\n\tΜέσος Όρος Περασμένων: " ++ (show $ fromIntegral (sum grades) / fromIntegral (length grades)) ++
-  "\n\n"
+moreInfo csv = concat [
+    "\nΓενικές Πληροφορίες:\n\n" 
+  , "\tΠερασμένα Κορμού: "  , (show $ l_p_k) 
+  , ", Απομένουν από Κορμό: ", (show $ 35 - l_p_k) 
+  , "\n\tΠερασμένα Ροών: " , (show $ l_p_r) 
+  , ", Απομένουν από Ροές: " , (show $ 22 - l_p_r) 
+  , "\n\tΠερασμένα Ελεύθερα: " , (show $ l_p_e) 
+  , ", Απομένουν από Ελεύθερα: " , (show $ 4 - l_p_e) 
+  , (concat (map grade [5,6,7,8,9,10])) 
+  , "\n\tΜέσος Όρος Περασμένων: "
+  , (show $ fromIntegral (sum grades) / fromIntegral (length grades)) 
+  , "\n\n"
+  ]
   where subjects = subjectsOf csv
-        grades = map (\x -> read $ element 4 from x) $ passed subjects
+        numGrades = \x -> element 4 from x `elem` map show [0..10]
+        grades = map (\x -> read $ element 4 from x) $ filter numGrades $ passed subjects
         grade n = concat ["\n\t",show n,"άρια: ",show $ length $ filter (== n) grades]
         l_p_k = length $ passedKormos subjects 
         l_p_r = length $ passedRoes subjects 
+        l_p_e = length $ passedElefthera subjects 
 
 main = do
   csv <- getContents
-  putStr $ concat [nicerKormos csv, nicerRoes csv, moreInfo csv]
+  putStr $ concat [nicerKormos csv, nicerRoes csv, nicerElefthera csv, moreInfo csv]
