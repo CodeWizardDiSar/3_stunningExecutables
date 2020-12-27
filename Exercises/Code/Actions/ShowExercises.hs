3f7ce96d28b374e5df1f0abac7e8d72be2bac146
gnostis <saridakisbitos@gmail.com>
2020-12-27 15:49:13 +0200

    I am an automated comment

--- Exercises/Code/Actions/ShowExercises.hs
+++ Exercises/Code/Actions/ShowExercises.hs
@@ -1,56 +1,42 @@
 module ShowExercises where
 import Prelude 
   ( Bool( True ), elem, (!!), filter, (>>=), (==), (-), (+), (>>), Int, IO, String, foldl
-  , otherwise )
+  , otherwise, (++) )
 import Types
   ( Exercise( exerciseData ), ExerciseData ( subjectName ), Exercises, Subject, Subjects
-  , toStringForUser )
+  , toStringForUser, toSubject )
 import Data.Function
   ( (&) )
 import Renaming 
-  ( glue, wrap, convertIntToString, printString, (>>>) )
+  ( forEach, glue, wrap, convertIntToString, printString, (>>>) )
 import Show 
   ( printEx )
 import UsefulForActions
   ( getChoice, showSubjects )
+import Choices
+  ( numbered )
+import UsefulFunctions
+  ( doSequentially )
 
 showExercises :: ( Exercises, Int ) -> IO ()
 showExercises = \( exs, subNum )->
-  let sub = getSubjects exs !! ( subNum - 1 )
+  let sub = toSubjects exs !! ( subNum - 1 )
   in filter (subIs sub) exs & printExercises
 
 subIs :: String -> Exercise -> Bool
-subIs = \subName -> getExerciseSubjectName >>> ( == subName )
-
-getExerciseSubjectName :: Exercise -> Subject
-getExerciseSubjectName = exerciseData >>> subjectName 
+subIs = \subName -> toSubject >>> ( == subName )
 
 printExercises :: Exercises -> IO ()
-printExercises = printExercise 1
+printExercises =
+  forEach toStringForUser >>> numbered >>> forEach printString >>> doSequentially
 
-printExercise :: Int -> Exercises -> IO ()
-printExercise = \i -> \case
-  [] -> wrap ()
-  ex:exs -> ( glue [ "\t", convertIntToString i, ": ", toStringForUser ex ] & printString ) >> 
-    printExercise ( i + 1 ) exs
+toSubjects :: Exercises -> Subjects
+toSubjects = foldl ( \subs ex -> addToSubjects ( toSubject ex ) subs ) [] 
 
-getSubjects :: Exercises -> Subjects
-getSubjects = \case
-  [] -> []
-  ex:exs ->
-    let sub = getExerciseSubjectName ex
-        subs = getSubjects exs
-    in elem sub subs & \case
-      True -> subs
-      _ -> sub:subs
-
-getSubjects2 :: Exercises -> Subjects
-getSubjects2 = foldl ( \subs ex -> getSubjects2h ( getExerciseSubjectName ex ) subs ) [] 
-
-getSubjects2h :: Subject -> Subjects -> Subjects
-getSubjects2h subject subjects
+addToSubjects :: Subject -> Subjects -> Subjects
+addToSubjects subject subjects
   | elem subject subjects = subjects
-  | otherwise = subject : subjects
+  | otherwise = subjects ++ [ subject ]
 
 getExsSubjects :: IO Exercises -> IO Int
 getExsSubjects getExs = getExs >>= showSubjects >> getChoice
